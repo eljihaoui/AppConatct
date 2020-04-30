74199915f6a0502c74757237a4248e8dec0e17ab
mohamed eljihaoui <mohamed.eljihaoui@taalim.ma>
2020-04-30 02:09:50 +0000

    DBContact

--- AppConatct.WinForms/Models/Contact.cs
+++ AppConatct.WinForms/Models/Contact.cs
@@ -1,12 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace AppConatct.WinForms.Models
 {
-   public class Contact
+    public class Contact
     {
         public int Id { get; set; }
         public string NomComplet { get; set; }
@@ -14,29 +10,21 @@ namespace AppConatct.WinForms.Models
         public string Email { get; set; }
         public string Tel { get; set; }
         public string Genre { get; set; }
-        public string UrlPhoto { get; set; }
+        public byte[] Photo { get; set; }
 
-        public Contact(string nomComplet, DateTime dateNaiss, string email, string tel, string genre, string urlPhoto)
+        public Contact(string nomComplet, DateTime dateNaiss, string email, string tel, string genre, byte[] Photo)
         {
             this.NomComplet = nomComplet;
             this.DateNaiss = dateNaiss;
             this.Email = email;
             this.Tel = tel;
             this.Genre = genre;
-            this.UrlPhoto = urlPhoto;
+            this.Photo = Photo;
         }
         public Contact()
         {
 
         }
-        public Contact( DateTime dateNaiss, string email, string tel, string genre, string urlPhoto)
-        {
-            this.DateNaiss = dateNaiss;
-            this.Email = email;
-            this.Tel = tel;
-            this.Genre = genre;
-            this.UrlPhoto = urlPhoto;
-        }
-
+      
     }
 }
---
+++ AppConatct.WinForms/Models/DBContact.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppConatct.WinForms.Models
+{
+    public static class DBContact
+    {
+        public static List<Contact> lstContacts;
+
+        // Ajouter un contact à la liste des contacts
+        public static int AddContact(Contact c)
+        {
+            if (lstContacts == null)
+            {
+                lstContacts = new List<Contact>();
+            }
+            c.Id = lstContacts.Any() ? lstContacts.Max(x => x.Id) + 1 : 100; // lambda Expressions
+            lstContacts.Add(c);
+            return c.Id;
+        }
+        // Récuperer  la liste des contacts
+        public static  List<Contact> GetLsitContacts()
+        {
+            return lstContacts;
+        }
+
+        // supprimer un contact 
+        public static void DeleteContact(int idContact)
+        {
+            int index = lstContacts.FindIndex(x => x.Id == idContact);
+            lstContacts.RemoveAt(index);
+        }
+
+        //Modifier un contact
+        public static void UpdateContact(Contact c)
+        {
+            Contact rech = lstContacts.FirstOrDefault(x => x.Id == c.Id);
+            rech.NomComplet = c.NomComplet;
+            rech.DateNaiss = c.DateNaiss;
+            rech.Email = c.Email;
+            rech.Genre = c.Genre;
+            rech.Tel = c.Tel;
+            rech.Photo = c.Photo;
+        }
+        //Chercher Par nom
+        public static List<Contact> SearchConatct(string name)
+        {
+            List<Contact> res = lstContacts.Where(x => x.NomComplet.ToLower().Contains(name.ToLower())).ToList();
+            return res;
+        }
+    }
+}
